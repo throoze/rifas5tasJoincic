58a133a6ab3286c07856b05ac83b5a7656e68537
Victor Manuel De Ponte Olivares <rdbvictor19@gmail.com>
2012-05-16 00:22:34 -0430

    Inicializado el proyecto. Añadidos patrones a ignorar.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rifas5tasJoincic::Application.config.secret_token = 'd6642ebfffbbd17bf1343cf1c9edfdd26055186215161349ad8b55d8ed304c7db1f044912576710b3991d05ae34b4a6cabae80b7e484569eaaf1f2caffc8e2e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rifas5tasJoincic::Application.config.session_store :cookie_store, key: '_rifas5tasJoincic_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rifas5tasJoincic::Application.config.session_store :active_record_store
